84a885fff85131a0799d6154120864e144bafd34
berke-bakar <berke.bakar@maximintegrated.com>
2023-03-29 10:06:06 +0300

    AstCreator class modified to generate and output graph into a file after our node visiting is over.

--- src/main/java/com/berkebakar/AstCreator/AstCreator.java
+++ src/main/java/com/berkebakar/AstCreator/AstCreator.java
@@ -1,24 +1,20 @@
 package com.berkebakar.AstCreator;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import org.apache.commons.text.StringEscapeUtils;
+import guru.nidi.graphviz.engine.*;
+import guru.nidi.graphviz.model.MutableGraph;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class AstCreator {
     public static void createAst(Path inputPath, Path outputPath) {
         try {
+            //TODO: Generalize the method to handle directories
             String sourceCode = Files.readString(inputPath);
             ASTParser parser = ASTParser.newParser(AST.JLS19);
             parser.setSource(sourceCode.toCharArray());
@@ -29,60 +25,28 @@ public class AstCreator {
             options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_17);
             parser.setCompilerOptions(options);
             parser.setResolveBindings(true);
+            parser.setBindingsRecovery(true);
             parser.setKind(ASTParser.K_CLASS_BODY_DECLARATIONS);
 
             TypeDeclaration typeDeclaration = (TypeDeclaration) parser.createAST(null);
+            GraphicalAstVisitor visitor = new GraphicalAstVisitor();
+            typeDeclaration.accept(visitor);
 
-            Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-            JsonObject json = new JsonObject();
-
-            // Add information about the method declaration
-            MethodDeclaration methodDeclaration = typeDeclaration.getMethods()[0];
-            json.addProperty("name", methodDeclaration.getName().getIdentifier());
-            json.addProperty("returnType", methodDeclaration.getReturnType2().toString());
-            json.addProperty("constructor", methodDeclaration.isConstructor());
-            json.addProperty("varargs", methodDeclaration.isVarargs());
-
-            List<String> modifiers = new ArrayList<>();
-            for (Object extendedModifier : methodDeclaration.modifiers()) {
-                IExtendedModifier modifier = (IExtendedModifier) extendedModifier;
-                if (modifier.isModifier()) {
-                    modifiers.add(modifier.toString());
-                }
+            //TODO: Make output type a command line argument with default value of PNG
+            Path outputFilePath = outputPath.resolve(inputPath.getFileName().toString().replace(".java", ".png"));
+            File outputFile = null;
+            if (Files.notExists(outputFilePath)){
+                outputFile = Files.createFile(outputFilePath).toFile();
             }
-            json.add("modifiers", gson.toJsonTree(modifiers));
-
-            // Add information about the method parameters
-            List<JsonObject> parameters = new ArrayList<>();
-            for (SingleVariableDeclaration parameter : (List<SingleVariableDeclaration>) methodDeclaration.parameters()) {
-                JsonObject parameterJson = new JsonObject();
-                parameterJson.addProperty("name", parameter.getName().getIdentifier());
-                parameterJson.addProperty("type", parameter.getType().toString());
-                parameters.add(parameterJson);
+            else {
+                outputFile = outputFilePath.toFile();
             }
-            json.add("parameters", gson.toJsonTree(parameters));
 
-            // Add information about the method body statements
-            Block body = methodDeclaration.getBody();
-            if (body != null) {
-                JsonArray statements = new JsonArray();
-                for (Object statement : body.statements()) {
-                    JsonObject statementJson = new JsonObject();
-                    statementJson.addProperty("type", statement.getClass().getSimpleName());
-                    statementJson.addProperty("text", StringEscapeUtils.unescapeJava(statement.toString().replaceAll("[\\t\\n\\r]+", "").translateEscapes()));
-                    statements.add(statementJson);
-                }
-                json.add("statements", statements);
-            }
-            String contentsJson = gson.toJson(json);
-            Path outputFilePath = outputPath.resolve(inputPath.getFileName().toString().replace(".java", ".json"));
+            MutableGraph astGraph = visitor.getGraph();
+            //TODO: Make width user defined from command line
+            //TODO: Make node type coloring configurable from a file
+            Graphviz.fromGraph(astGraph).width(1920).height(1080).render(Format.PNG).toFile(outputFile);
 
-            if (!Files.exists(outputFilePath)){
-                Files.createFile(outputFilePath);
-            }
-            Files.write(outputFilePath, contentsJson.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
-
-            System.out.printf("AST exported successfully as %s to %s%n", outputFilePath.getFileName().toString(), outputPath.toRealPath());
         } catch (IOException e) {
             System.err.println("An error occurred while writing to file: " + e.getMessage());
         }

05c42ea3ad34d8f0333490c2cd08e2ee2aa162ac
Berke Bakar <berkebakar97@gmail.com>
2023-03-30 19:32:26 +0300

    Added support for properties file in Main.java, also if the user deletes config.properties, it will be created automatically

--- src/main/java/com/berkebakar/AstCreator/Main.java
+++ src/main/java/com/berkebakar/AstCreator/Main.java
@@ -2,11 +2,12 @@ package com.berkebakar.AstCreator;
 
 import org.apache.commons.cli.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 public class Main {
@@ -14,6 +15,14 @@ public class Main {
     public static void main(String[] args) {
         CommandLineParser cliParser = new DefaultParser();
         Options cliOptions = createCliOptions();
+        // Read or create properties file
+        System.out.println("Reading config.properties...");
+        Properties properties = getOrCreateProperties();
+
+        if (properties == null){
+            System.err.println("config.properties file cannot be created, move the executable to a different location, exiting...");
+            System.exit(7);
+        }
 
         try {
             CommandLine commandLine = cliParser.parse(cliOptions, args);
@@ -42,7 +51,7 @@ public class Main {
                                 .filter(path -> path.toString().endsWith(".java"))
                                 .forEach(path -> {
                                     System.out.println("Creating AST for " + path.getFileName().toString());
-                                    AstCreator.createAst(path, outputPath);
+                                    AstCreator.createAst(path, outputPath, properties);
                                     System.out.println("Generated AST for " + path.getFileName().toString());
                                 }); // Replace with your own logic
                     } catch (IOException e) {
@@ -52,7 +61,7 @@ public class Main {
                 }
                 else { // single file
                     System.out.println("Creating AST for " + inputPath.getFileName().toString());
-                    AstCreator.createAst(inputPath, outputPath);
+                    AstCreator.createAst(inputPath, outputPath, properties);
                     System.out.println("Generated AST for " + inputPath.getFileName().toString());
                 }
             }
@@ -60,8 +69,6 @@ public class Main {
                 System.err.println("Input path is null, exiting...");
                 System.exit(5);
             }
-
-            AstCreator.createAst(inputPath, outputPath);
         } catch (ParseException e) {
             System.err.println(e.getMessage());
             System.exit(4);
@@ -146,6 +153,82 @@ public class Main {
         for (Option option : createCliOptions().getOptions()){
             System.out.printf("-%-5s --%-10s      %-50s\n", option.getOpt(), option.getLongOpt(), option.getDescription());
         }
+    }
+
+    private static Properties getOrCreateProperties(){
+        Properties props = new Properties();
+        try (InputStream inputStream = new FileInputStream("config.properties")) {
+            props.load(inputStream);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find config.properties, creating a new one...");
+            // The file doesn't exist, so create it with default values
+            props.setProperty("output.height", "224");
+            props.setProperty("output.width", "224");
+            props.setProperty("output.detailed", "false");
+            props.setProperty("output.fillNodes", "true");
+            // Default shape values
+            props.setProperty("TypeDeclaration.shape", "true");
+            props.setProperty("MethodDeclaration.shape", "box");
+            props.setProperty("AnonymousClassDeclaration.shape", "house");
+            props.setProperty("SingleVariableDeclaration.shape", "circle");
+            props.setProperty("AssertStatement.shape", "star");
+            props.setProperty("Block.shape", "square");
+            props.setProperty("BreakStatement.shape", "trapezium");
+            props.setProperty("ContinueStatement.shape", "invtriangle");
+            props.setProperty("DoStatement.shape", "hexagon");
+            props.setProperty("EnhancedForStatement.shape", "parallelogram");
+            props.setProperty("ExpressionStatement.shape", "triangle");
+            props.setProperty("ForStatement.shape", "parallelogram");
+            props.setProperty("IfStatement.shape", "diamond");
+            props.setProperty("LabeledStatement.shape", "pentagon");
+            props.setProperty("ReturnStatement.shape", "cds");
+            props.setProperty("SwitchStatement.shape", "diamond");
+            props.setProperty("SynchronizedStatement.shape", "octagon");
+            props.setProperty("ThrowStatement.shape", "invtrapezium");
+            props.setProperty("TryStatement.shape", "invhouse");
+            props.setProperty("CatchClause.shape", "septagon");
+            props.setProperty("TypeDeclarationStatement.shape", "doubleoctagon");
+            props.setProperty("VariableDeclarationStatement.shape", "component");
+            props.setProperty("WhileStatement.shape", "hexagon");
+            props.setProperty("YieldStatement.shape", "rarrow");
+            // Default color values
+            props.setProperty("TypeDeclaration.color", "green");
+            props.setProperty("MethodDeclaration.color", "aqua");
+            props.setProperty("AnonymousClassDeclaration.color", "darkgoldenrod1");
+            props.setProperty("SingleVariableDeclaration.color", "darkgreen");
+            props.setProperty("AssertStatement.color", "teal");
+            props.setProperty("Block.color", "coral");
+            props.setProperty("BreakStatement.color", "orange");
+            props.setProperty("ContinueStatement.color", "sienna");
+            props.setProperty("DoStatement.color", "yellow");
+            props.setProperty("EnhancedForStatement.color", "red");
+            props.setProperty("ExpressionStatement.color", "fuchsia");
+            props.setProperty("ForStatement.color", "red");
+            props.setProperty("IfStatement.color", "purple");
+            props.setProperty("LabeledStatement.color", "lightpink");
+            props.setProperty("ReturnStatement.color", "hotpink");
+            props.setProperty("SwitchStatement.color", "purple");
+            props.setProperty("SynchronizedStatement.color", "palegreen");
+            props.setProperty("ThrowStatement.color", "tan");
+            props.setProperty("TryStatement.color", "honeydew4");
+            props.setProperty("CatchClause.color", "firebrick");
+            props.setProperty("TypeDeclarationStatement.color", "royalblue");
+            props.setProperty("VariableDeclarationStatement.color", "brown");
+            props.setProperty("WhileStatement.color", "yellow");
+            props.setProperty("YieldStatement.color", "blue");
+            // Write to file
+            try (OutputStream outputStream = new FileOutputStream("config.properties")) {
+                props.store(outputStream, "Default properties file created by AstCreator4j");
+            } catch (IOException ex) {
+                // Handle the exception
+                return null;
+            }
+        } catch (IOException e) {
+            // Could not create or write to file
+            return null;
+        }
+
+        return props;
     }
 
 }
